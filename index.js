03ec8048260f88a5f5fd1e90c0c524ebd41e3fca
AkashMAbrrar <madborakash48@gmail.com>
2025-01-17 13:15:02 +0600

    This is the first commit for set the project and added  to the mongodb database

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c4n3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const classCollectionTeacher = client.db("EduManage").collection("classes");
+    app.get("/classes", async (req, res) => {
+      const result = await classCollectionTeacher.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// root path
+app.get("/", (req, res) => {
+  res.send("Teacher And Admin Is Going...");
+});
+
+app.listen(port, () => {
+  console.log(`Ed Manage Is Running On Port: ${port}`);
+});
