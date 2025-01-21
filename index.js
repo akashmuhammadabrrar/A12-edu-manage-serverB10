476d744b31f116ad86aca7c5ae16428673d61615
AkashMAbrrar <madborakash48@gmail.com>
2025-01-21 13:50:27 +0600

    this commit for create a payment api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 
@@ -216,6 +217,21 @@ async function run() {
     app.get("/teacher-req", verifyToken, async (req, res) => {
       const result = await teacherReqCollection.find().toArray();
       res.send(result);
+    });
+
+    // payment related api
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
